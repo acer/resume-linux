6fad1097b86eeb8194eddb26fed143b0cdcaa7a8
Acer Wang <acer.chi.wang@gmail.com>
2015-03-28 19:49:52 -0400

    getopt_long structure ready

--- acerwang.c
+++ acerwang.c
@@ -1,36 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
 #include <getopt.h>
 
-int main (int argc, char **argv)
+void print_all();
+void print_contact();
+void print_edu(char * option);
+void print_getpdf();
+void print_help();
+void print_asciiart();
+void print_other();
+void print_projects(char * option);
+void print_skill();
+void print_volun();
+void print_work(char * option);
+
+
+int main (int argc, char *argv[])
 {
 	
-	int c;
-	if (argc > 0)
+	if (argc > 1)
 	{
-		while(1)
+		int opt; 
+		int long_index = 0;
+		extern char *optarg;
+		extern int optopt, opterr, optind;
+
+		static struct option longopt[] =
 		{
-			static struct option long_options[] =
-			{
-				{"all", no_argument, 0, 'a'},
-				{"contact", no_argument, 0, 'c'},
-				{"education", optional_argument, 0, 'e'},
-				{"getpdf", no_argument, 0, 'g'},
-				{"help", no_argument, 0, 'h'},
-				{"me", no_argument, 0, 'm'},
-				{"other", no_argument, 0, 'o'},
-				{"projects", optional_argument, 'p'},
-				{"skill", no_argument, 's'},
-				{"volunteer", no_argument, 'v'},
-				{"work", optional_argument, 'w'},
-				{0,0,0,0}
-			};
-				exit(0);
-		}
+			{"all",       no_argument,       NULL, 'a'},
+			{"contact",   no_argument,       NULL, 'c'},
+			{"education", optional_argument, NULL, 'e'},
+			{"getpdf",    no_argument,       NULL, 'g'},
+			{"help",      no_argument,       NULL, 'h'},
+			{"me",        no_argument,       NULL, 'm'},
+			{"other",     no_argument,       NULL, 'o'},
+			{"projects",  optional_argument, NULL, 'p'},
+			{"skill",     no_argument,       NULL, 's'},
+			{"volunteer", no_argument,       NULL, 'v'},
+			{"work",      optional_argument, NULL, 'w'},
+			{0,           0,                 0,      0}
+		}; //end long_options
+
+		while(opt = getopt_long(argc, argv, "ace::ghop::svw::", longopt, &long_index) != -1)
+		{
+			printf("opt=%d, optind = %d, opterr=%d, optopt=%d\n", opt, optind, opterr, optopt);
+			switch (opt) {
+				case 'a':
+					print_all();
+					break;
+
+				case 'c':
+					print_contact();
+					break;
+
+				case 'e':
+					print_edu(optarg);
+					break;
+
+				case 'g':
+					print_getpdf();
+					break;
+
+				case 'h':
+					print_help();
+					break;
+
+				case 'm':
+					print_asciiart();
+					break;
+
+				case 'o':
+					print_other();
+					break;
+
+				case 'p':
+					print_projects(optarg);
+					break;
+
+				case 's':
+					print_skill();
+					break;
+
+				case 'v':
+					print_volun();
+					break;
+
+				case 'w':
+					print_work(optarg);
+					break;
+
+				case '?':
+					fprintf(stderr, "-%c is not a valid option.\n", opt);
+
+			}//end switch
+
+		}//while(c != 1)
+		exit(0);
 	} else {
 		printf("Tell me what you want to know, okay?\n");
 		exit(0);
-	}
-;
+	} //end else
+
+}//main 
+
+void print_all(){
+
+}
+
+void print_contact(){
+
+}
+
+void print_edu(char * option){
+
+}
+
+void print_getpdf(){
+
+}
+
+void print_help(){
+
+}
+
+void print_asciiart(){
+
+}
+
+void print_other(){
+
+}
+
+void print_projects(char * option){
+
+}
+
+void print_skill(){
+
+}
+
+void print_volun(){
+
+}
+
+void print_work(char * option){
 
 }
